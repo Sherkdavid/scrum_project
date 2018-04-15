159e7d986f828ea29e9e74958f4b72d5c2e993e7
david <dmurphy10@mycit.ie>
2018-04-15 20:51:42 +0100

    Changed message text

--- web_time.py
+++ web_time.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     localtime = time.localtime(time.time())
-    return 'Hello World, it is '+str(time.asctime(localtime))
+    return 'Hello World, the time and data is '+str(time.asctime(localtime))
 
 
 if __name__ == '__main__':
